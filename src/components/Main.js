a3fcb526bb819db81a0b17ed14eba320e18f42b3
Abhay <26abhaykumarsingh@gmail.com>
2024-01-31 00:52:05 +0530

    ADD : Main, file-uploader

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,90 @@
 import "./Main.css";
 
 import React, { useState } from "react";
+import * as XLSX from "xlsx";
 import Sidebar from "./Sidebar";
+import Table from "./Table";
 
 function Main() {
+  const [file, setFile] = useState(null);
+  const [extractedData, setExtractedData] = useState(null);
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+
+    const droppedFiles = event.dataTransfer.files;
+    setFile(droppedFiles[0]);
+    console.log(droppedFiles[0]);
+  };
+
+  const handleFileInput = (event) => {
+    const selectedFiles = event.target.files;
+    setFile(selectedFiles);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleRemoveFile = () => {
+    setFile(null);
+  };
+
+  const handleFile = () => {
+    const selectedFile = file;
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
+
+      // Assuming you want to read the first sheet
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+
+      // Convert sheet data to JSON
+      const jsonData = XLSX.utils.sheet_to_json(sheet);
+
+      console.log(jsonData);
+      setExtractedData(jsonData);
+    };
+
+    reader.readAsArrayBuffer(selectedFile);
+  };
+
   return (
     <div className="Main">
       <Sidebar></Sidebar>
-      <div className="mainHeader">Upload CSV</div>
+      <div className="mainContainer">
+        <div className="mainHeader">Upload CSV</div>
+        <div className="fileUploadContainer">
+          <div
+            className="dropArea"
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+          >
+            <div className="excelIcon"></div>
+            {!file && (
+              <div className="dropAreaText">
+                Drop your excel sheet here or <a href="">browse</a>
+              </div>
+            )}
+            {file && (
+              <div className="selectedFilesContainer">
+                {file.name}
+                <div className="removeText" onClick={() => handleRemoveFile()}>
+                  Remove
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="uploadBtn" onClick={handleFile}>
+            <div className="uploadBtnIcon"></div>
+            <div className="uploadBtnText">Upload</div>
+          </div>
+        </div>
+        {extractedData && <Table></Table>}
+      </div>
     </div>
   );
 }
---
+++ src/components/Table.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Table.css";
+
+function Table() {
+  return (
+    <div className="Table">
+      <div className="tableHeading">Uploads</div>
+    </div>
+  );
+}
+
+export default Table;
